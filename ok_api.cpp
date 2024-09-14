56887f2a60976c320620319269c13375267fff76
Wasser Bauer <wb04011998@gmail.com>
2024-09-14 18:18:22 +0700

    my commit

--- ok_api.cpp
+++ ok_api.cpp
@@ -23,7 +23,7 @@ public:
     void handle_get(http_request request) {
         ucout << "Received GET request" << std::endl;
         json::value response;
-        response[U("message")] = json::value::string(U("OK"));
+        response[U("message")] = json::value::string(U("Updated"));
         request.reply(status_codes::OK, response);
     }
 
